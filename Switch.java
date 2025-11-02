8f886497a41602e8a2e40970f21385cff5326479
eya113 <eyarmedi@gmail.com>
2025-11-02 15:10:15 +0100

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,22 @@
+package lesswitch;
+
+public class Switch {
+    public static void main(String[] args) {
+        int dayOf = 5;
+
+        switch (dayOf) {
+            case 0:
+                System.out.println("sun");
+               
+
+            case 6:
+                System.out.println("suf");
+              
+
+            default:
+                System.out.println("sul");
+                break;
+        }
+    }
+}
+
